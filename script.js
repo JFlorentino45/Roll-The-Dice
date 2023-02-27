3e86c3ae8581a82def9e27e6f62737aba92506db
Jarryd <Jarryd.Florentino@gmail.com>
2023-02-27 21:00:48 +0100

    Finished starting screen

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-"use strict";
+'use strict';
+
+const score0Ele = document.querySelector('#score--0');
+const score1Ele = document.querySelector('#score--1');
+const diceEle = document.querySelector('.dice');
+
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
+diceEle.classList.add('hidden');
