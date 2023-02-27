071cf5d14f5e1b1d85ce8b578369ba9162954d1c
Jarryd <Jarryd.Florentino@gmail.com>
2023-02-27 22:00:54 +0100

    added player change and current scoring logic

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 'use strict';
 
+const player0Ele = document.querySelector('.player--0');
+const player1Ele = document.querySelector('.player--1');
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.querySelector('#score--1');
+const current0Ele = document.querySelector('#current--0');
+const current1Ele = document.querySelector('#current--1');
 const diceEle = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
-score0Ele.textContent = 0;
-score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
 
 btnRoll.addEventListener('click', function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   diceEle.classList.remove('hidden');
   diceEle.src = `./images/dice-${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Ele.classList.toggle('player--active');
+    player1Ele.classList.toggle('player--active');
+  }
 });
