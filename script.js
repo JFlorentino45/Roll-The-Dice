646dffc0b7ed7003950e89e1e14f1798e5b1dc36
Jarryd <Jarryd.Florentino@gmail.com>
2023-02-27 22:40:45 +0100

    added player toal score and winning logic

--- script.js
+++ script.js
@@ -16,6 +16,14 @@ let activePlayer = 0;
 
 diceEle.classList.add('hidden');
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Ele.classList.toggle('player--active');
+  player1Ele.classList.toggle('player--active');
+};
+
 btnRoll.addEventListener('click', function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   diceEle.classList.remove('hidden');
@@ -25,10 +33,21 @@ btnRoll.addEventListener('click', function () {
     currentScore += diceNumber;
     document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+  if (scores[activePlayer] >= 20) {
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    diceEle.classList.add('hidden');
     document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0Ele.classList.toggle('player--active');
-    player1Ele.classList.toggle('player--active');
+    document.querySelector('.btn--roll').style.visibility = 'hidden';
+    document.querySelector('.btn--hold').style.visibility = 'hidden';
+  } else {
+    switchPlayer();
   }
 });
